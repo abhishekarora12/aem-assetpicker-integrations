1a78452668ff3935308f243c40c134b5aea00d58
Abhishek Arora <aarora@adobe.com>
2022-11-08 20:04:43 +0530

    set default width for assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '825bb862287894dc1402');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'aa58238e124316e77826');
