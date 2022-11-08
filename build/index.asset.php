3c485887df2d6d18e53d0c7718bfd5a6d1b6ec8c
Abhishek Arora <aarora@adobe.com>
2022-11-08 20:10:04 +0530

    update default width for assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'aa58238e124316e77826');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0055d1c8fb43d9087531');
